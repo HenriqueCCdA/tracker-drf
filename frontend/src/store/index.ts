cde3547d564baaf75739aab05d1b697e332102ee
Henrique Andrade <henrique.ccda@gmail.com>
2023-05-04 01:22:24 -0300

    Iniciando o projeto tracker fullstack

---
+++ frontend/src/store/index.ts
@@ -0,0 +1,45 @@
+import { createStore, Store, useStore as vuexUseStore } from "vuex";
+import { InjectionKey } from 'vue'
+import { NOTIFICAR } from "./tipo-mutacoes";
+import { INotificacao } from "@/interfaces/INotificacao";
+import { EstadoProjeto, projeto } from "./modulos/projetos";
+import { EstadoTarefa, tarefa } from "./modulos/tarefas";
+
+export interface Estado {
+    tarefa: EstadoTarefa,
+    notificacoes: INotificacao[],
+    projeto: EstadoProjeto,
+}
+
+export const key: InjectionKey<Store<Estado>> = Symbol()
+
+export const store = createStore<Estado>({
+    state: {
+        tarefa: {
+            tarefas: []
+        },
+        notificacoes: [],
+        projeto: {
+            projetos: []
+        }
+    },
+    mutations: {
+        [NOTIFICAR] (state, novaNotificacao: INotificacao) {
+
+            novaNotificacao.id = new Date().getTime()
+            state.notificacoes.push(novaNotificacao)
+
+            setTimeout(() => {
+                state.notificacoes = state.notificacoes.filter(notificacao => notificacao.id != novaNotificacao.id)
+            }, 3000)
+        }
+    },
+    modules: {
+        projeto,
+        tarefa,
+    }
+})
+
+export function useStore(): Store<Estado> {
+    return vuexUseStore(key)
+}
